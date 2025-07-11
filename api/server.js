a4619dcc0b33617eab6424be188995ba37f20666
Aravind12200363 <aravindchowdary8082@gmail.com>
2025-07-11 16:05:26 +0530

    updated

--- api/server.js
+++ api/server.js
@@ -1,31 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/userRoute.js";
-import cors from "cors"
+import userRouter from "../routes/userRoute.js";
+import cors from "cors";
+import serverless from "serverless-http";
+
 dotenv.config();
+
 const app = express();
-app.use(cors())
+
+app.use(cors());
 app.use(express.json());
+
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
-
-// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
 
 mongoose
   .connect(
     `mongodb+srv://${dbuser}:${dbpass}@cluster0.us1ya.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    app.listen(8080, () => {
-      console.log("Server started");
-    });
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
   });
 
-
-
 app.use("/api/users", userRouter);
+
+// 🔁 Export as a serverless handler
+export const handler = serverless(app);
